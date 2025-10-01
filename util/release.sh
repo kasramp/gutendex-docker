c5c90f7adaf1571716d3d8e8bb69f331d4e3d1de
kasramp <kasra_mp@yahoo.com>
2025-10-01 13:54:32 +0000

    Fix the pipeline

--- util/release.sh
+++ util/release.sh
@@ -11,7 +11,13 @@ IMAGE_NAME=kasramp/gutendex
 docker login -u "$DOCKER_USERNAME" --password-stdin <<< "$DOCKER_PASSWORD" &&
 docker buildx create --use &&
 docker buildx build --pull --platform linux/amd64,linux/arm64 --build-arg VERSION="$TAGGED_VERSION" -t "$IMAGE_NAME":latest -t "$IMAGE_NAME:$TAGGED_VERSION" . --push
-git add "version.txt" &&
-git commit -m "Update version file to $TAGGED_VERSION" &&
-git push origin HEAD:master &&
-echo "Successfully build and pushed Docker $TAGGED_VERSION to Docker Hub"
+
+if git diff --quiet && git diff --cached --quiet; then
+  echo "No changes to commit." &&
+  echo "Successfully build and pushed Docker $TAGGED_VERSION to Docker Hub"
+else
+  git add "version.txt" &&
+  git commit -m "Update version file to $TAGGED_VERSION" &&
+  git push origin HEAD:master &&
+  echo "Successfully build and pushed Docker $TAGGED_VERSION to Docker Hub"
+fi
